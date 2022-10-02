2744c2882b82ea92529fa217ec1d244f14f85432
Oliver Schmidt <oliverpierre.schmidt@gmail.com>
2022-10-03 01:03:33 +0200

    Add ClassTest

---
+++ src/BlazingDocumentor/BlazingDocumentor.Test/CLassTest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = BlazingDocumentor.Test.CSharpCodeFixVerifier<
+    BlazingDocumentor.ClassAnalyzer,
+    BlazingDocumentor.ClassCodeFixProvider>;
+
+namespace BlazingDocumentor.Test
+{
+    [TestClass]
+    public class CLassTest
+    {
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var test = @"";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var test = @"
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+	public class SuperDuperClassTester
+	{
+	}
+}";
+
+            var fixtest = @"
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    /// <summary>
+    /// The super duper class tester.
+    /// </summary>
+    public class SuperDuperClassTester
+	{
+	}
+}";
+
+            var expected = VerifyCS.Diagnostic(ClassAnalyzer.DiagnosticId)
+                .WithLocation(8, 15)
+                .WithSeverity(Microsoft.CodeAnalysis.DiagnosticSeverity.Warning)
+                .WithMessage(ClassAnalyzer.MessageFormat);
+            await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
+        }
+    }
+}
